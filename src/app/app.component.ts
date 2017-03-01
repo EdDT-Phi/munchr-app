9575fd6684363210bcc887241acf66f3c4ef08ba
Eddie <eddt.phi@gmail.com>
2017-02-28 19:51:20 -0600

    image paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Login } from "../pages/login/login";
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Main;
+  rootPage: any = Login;
 
   pages: Array<{title: string, component: any}>;
 
