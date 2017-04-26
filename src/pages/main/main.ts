6bd510490f4bbd34d9640b5a7a986ae685e0769c
Eddie <eddt.phi@gmail.com>
2017-04-25 20:17:28 -0500

    user

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -49,10 +49,10 @@ export class Main {
 
 		}, error => {
 			// Not logged in
-			// this.get_user();
+			this.get_user();
 
-			this.user = {user_id: 3, first_name:'Tyler', last_name:'Camp', photo_url:''}
-			this.after_get_user();
+			// this.user = {user_id: 3, first_name:'Tyler', last_name:'Camp', photo_url:''}
+			// this.after_get_user();
 		});
 
 
